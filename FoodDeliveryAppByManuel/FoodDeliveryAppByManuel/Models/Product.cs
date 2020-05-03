a085e77ff945973eaacc654bfabad3967eb1ec7f
wstardev <wstar0506@gmail.com>
2020-05-03 13:00:48 +0200

    Deleted unnecessary libraries in the models folder for optimization

--- FoodDeliveryAppByManuel/FoodDeliveryAppByManuel/Models/Product.cs
+++ FoodDeliveryAppByManuel/FoodDeliveryAppByManuel/Models/Product.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FoodDeliveryAppByManuel.Models
+﻿namespace FoodDeliveryAppByManuel.Models
 {
     public class Product
     {
