309625a84a728ce1ff80e5dc3a5459d54a65dbce
wstardev <wstar0506@gmail.com>
2020-05-03 17:30:56 +0200

    Added Login and added FoodApi

--- FoodDeliveryAppByManuel/FoodDeliveryAppByManuel/Services/ApiService.cs
+++ FoodDeliveryAppByManuel/FoodDeliveryAppByManuel/Services/ApiService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace FoodDeliveryAppByManuel.Services
 {
@@ -21,8 +22,29 @@ namespace FoodDeliveryAppByManuel.Services
             var httpClient = new HttpClient();
             var json = JsonConvert.SerializeObject(register);
             var content =new StringContent(json, Encoding.UTF8, "aplication/json");
-            var response = await httpClient.PostAsync("direccionConveyorDeMiAPI/api/Accounts/Register", content);
+            var response = await httpClient.PostAsync("http://FoodApi/api/Accounts/Register", content);
             if (!response.IsSuccessStatusCode) return false;
+            return true;
+        }
+
+        public static async Task<bool> Login(string email, string password)
+        {
+            var login = new Login()
+            {
+                Email = email,
+                Password = password
+            };
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(login);
+            var content = new StringContent(json, Encoding.UTF8, "aplication/json");
+            var response = await httpClient.PostAsync("http://FoodApi/api/Accounts/Login", content);
+            if (!response.IsSuccessStatusCode) return false;
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Token>(jsonResult);
+            Preferences.Set("accessToken", result.access_token);
+            Preferences.Set("accessToken", result.user_Id);
+            Preferences.Set("accessToken", result.user_name);
+
             return true;
         }
     }
