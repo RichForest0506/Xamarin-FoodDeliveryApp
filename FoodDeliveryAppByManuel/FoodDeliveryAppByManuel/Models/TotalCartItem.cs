db38f2d24a4e04c60ccd3d137102f8d80f204832
wstardev <wstar0506@gmail.com>
2020-05-03 12:39:10 +0200

    Added Model For Shopping Cart Items

---
+++ FoodDeliveryAppByManuel/FoodDeliveryAppByManuel/Models/TotalCartItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodDeliveryAppByManuel.Models
+{
+    public class TotalCartItem
+    {
+        public int totalItems { get; set; }
+    }
+}
